c06add051afddf505c324dd483a0d03b7c348d46
Himanshu Singh <himanshusingh1k@gmail.com>
2025-11-16 13:08:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your code here
+// Write your code here:
+const tBody = document.querySelector("tbody");
+const newRow = document.createElement("tr");
+tBody.append(newRow);
+
+const newData1 = document.createElement("td");
+const button = document.createElement("button");
+button.innerText = "Total";
+newData1.appendChild(button);
+
+const newData2 = document.createElement("td");
+newData2.setAttribute("id", "ans");
+newData2.setAttribute("data-ns-test", "grandTotal");
+
+newRow.append(newData1);
+newRow.append(newData2);
+
+button.addEventListener("click", () => {
+  const prices = document.querySelectorAll(".price");
+  let sum = 0;
+  for (let price of prices) sum += Number(price.innerText) || 0;
+  newData2.innerText = sum;
+});
