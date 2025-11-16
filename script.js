c89f390d60683739ea19ee9a4ff4f1bc683c5917
Himanshu Singh <himanshusingh1k@gmail.com>
2025-11-16 13:30:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Write your code here:
 const tBody = document.querySelector("tbody");
 const newRow = document.createElement("tr");
 tBody.append(newRow);
@@ -6,7 +5,7 @@ tBody.append(newRow);
 const newData1 = document.createElement("td");
 const button = document.createElement("button");
 button.innerText = "Total";
-button.style.cursor = "pointer"
+button.style.cursor = "pointer";
 newData1.appendChild(button);
 
 const newData2 = document.createElement("td");
@@ -16,9 +15,11 @@ newData2.setAttribute("data-ns-test", "grandTotal");
 newRow.append(newData1);
 newRow.append(newData2);
 
-button.addEventListener("click", () => {
-  const prices = document.querySelectorAll(".price");
+function calculateSum() {
+  const prices = document.querySelectorAll("[data-ns-test='price']");
   let sum = 0;
-  for (let price of prices) sum += Number(price.innerText) || 0;
-  newData2.innerText = String(sum);
-});
+  for (let price of prices) sum += parseInt(price.textContent, 10) || 0;
+  newData2.textContent = sum.toString();
+}
+button.addEventListener("click", calculateSum);
+calculateSum();
